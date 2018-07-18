6472170d2a048f53fd2d5f94de0872e30df9107d
mollekaplan <molly.elizabeth.kaplan@cern.ch>
2018-07-18 17:09:48 -0400

    Include citation of F(x)

--- bessel.py
+++ bessel.py
@@ -5,9 +5,10 @@ Created on Thu Jun 21 10:42:08 2018
 
 @author: mollykaplan
 
-Calculates F(x)=x*int_x^(infinity) K_(5/3)(y) dy for various values 
-of x. The function Fx outputs a tuple with the x values in the first
-index and F(x) values in the second.
+Calculates F(x)=x*int_x^(infinity) K_(5/3)(y) dy, as given by
+Marscher et. al. 1978 Eq. 16, for various values of x. The 
+function Fx outputs a tuple with the x values in the first index 
+and F(x) values in the second.
 """
 
 import numpy as np
@@ -17,7 +18,6 @@ import scipy.integrate as integrate
 #creating F(x) function
 def mod_bessel(x):
     return x*integrate.quad(lambda xi: kv(5/3,xi), x, float("inf"))[0]
-
 
 #range of values for x, equally spaced in log space
 x=np.geomspace(1e-5,1e2,100)
@@ -30,3 +30,4 @@ for elt in x: F.append(mod_bessel(elt))
 #outputting a tuple with arrays of (1) x values and (2) F values
 def Fx():
     return (x,F)
+
